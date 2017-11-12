f0ce0c6def41306258b9da47fe423402218eb5c9
stonycox <s.tonycox@gmail.com>
2017-11-12 16:12:40 +0530

    first commit

---
+++ media.py
@@ -0,0 +1,11 @@
+#Created By : Tony
+#Created On : 11/12/2017
+#Description: This file contains Movie class that defines properties and functions required for Movie trailer website
+
+class Movie():
+	def __init__(self,title,story_line,poster_image_url,trailer_youtube_url):
+		self.title = title
+		self.story_line = story_line
+		self.poster_image_url = poster_image_url
+		self.trailer_youtube_url = trailer_youtube_url
+
