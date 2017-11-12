253f1a0f327825fb9e51d8d32f5a36842269a194
stonycox <s.tonycox@gmail.com>
2017-11-12 21:24:32 +0530

    Indented the file using PEP8 online tool

--- media.py
+++ media.py
@@ -1,11 +1,18 @@
-#Created By : Tony
-#Created On : 11/12/2017
-#Description: This file contains Movie class that defines properties and functions required for Movie trailer website
+# Created By : Tony
+# Created On : 11/12/2017
+# Description: This file contains Movie class that defines properties and
+#              functions required for Movie trailer website
+
 
 class Movie():
-	def __init__(self,title,story_line,poster_image_url,trailer_youtube_url):
-		self.title = title
-		self.story_line = story_line
-		self.poster_image_url = poster_image_url
-		self.trailer_youtube_url = trailer_youtube_url
+    """This movie class defines structure of data that we are going
+    to store for each movie"""
 
+    def __init__(self, title, story_line, poster_image_url,
+                 trailer_youtube_url):
+        """This is the constructor method of movie class.
+        It initializes the movie class properties when invoked"""
+        self.title = title
+        self.story_line = story_line
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
